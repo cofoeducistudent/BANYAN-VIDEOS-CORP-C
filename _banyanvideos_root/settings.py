6c682a7a46380b04f40c9403395942f1eb19fb58
cofoeducistudent <cofoedu91@gmail.com>
2020-11-05 16:46:41 +0000

    Initial Commit

--- _banyanvideos_root/settings.py
+++ _banyanvideos_root/settings.py
@@ -81,6 +81,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
+        
+        
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -116,7 +118,7 @@ LOGIN_REDIRECT_URL ='/'
 
 WSGI_APPLICATION = '_banyanvideos_root.wsgi.application'
 
-DISABLE_COLLECTSTATIC = 1
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -150,7 +152,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
