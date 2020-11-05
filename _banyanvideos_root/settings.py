e204285f0fe5f486e533ad5c83b4f36cc58c6529
cofoeducistudent <cofoedu91@gmail.com>
2020-11-05 16:11:21 +0000

    Initial commit

--- _banyanvideos_root/settings.py
+++ _banyanvideos_root/settings.py
@@ -116,7 +116,7 @@ LOGIN_REDIRECT_URL ='/'
 
 WSGI_APPLICATION = '_banyanvideos_root.wsgi.application'
 
-DISABLE_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC = 1
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
