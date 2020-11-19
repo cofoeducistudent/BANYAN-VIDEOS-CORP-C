7577665e87e26b91ed5f7f0cc021c3e55c302dbd
cofoeducistudent <cofoedu91@gmail.com>
2020-11-19 21:21:02 +0000

    fixed cart count on search result page

--- search_results/views.py
+++ search_results/views.py
@@ -13,38 +13,36 @@ from .import models as film_models
 
 from django.contrib import messages
 
-from shopping_cart.models  import ShoppingCartModel
+from shopping_cart.models import ShoppingCartModel
 
 # Create your views here.
 
+
 class SearchResults(TemplateView):
-    
+
     try:
-    
+
         template_name = 'search_results/search_results.html'
 
         """
         GET GENRE
         """
-    
+
         def get(self, request):
-            
-            
-        # CREATE A SESSION IF NOT EXISTING!!
+
+            # CREATE A SESSION IF NOT EXISTING!!
             if not request.session.exists(request.session.session_key):
                 request.session.create()
             session_key = request.session.session_key
-        
+
             current_user = request.user
             if not request.user.is_authenticated: 
                 current_user = request.user
             if request.user.is_authenticated:
-                current_user = request.user.username         
-            
-            
-            
+                current_user = request.user
+
             results_collection = []
-       
+
             all_films = film_models.Films.objects.all()
 
             sr = request.GET.get('sel_genre')
@@ -53,28 +51,33 @@ class SearchResults(TemplateView):
                     results_collection.append(items)
 
             total_items_found = len(results_collection)
-        
-        
-        
-        
+
             SCM = ShoppingCartModel.objects.filter(cart_owner=current_user)
             basket_item_count = SCM.count
 
-        
-        
-        
-        
-        
+      
+
+
+
+
+
+
             context = {
                 'total_items_found': total_items_found,
                 'results_collection': results_collection,
-            
-                'basket_item_count':basket_item_count,
-                'current_user':current_user,
-                'SCM':SCM,
-        
+
+                'basket_item_count': basket_item_count,
+                'current_user': current_user,
+                
+                'SCM': SCM,
+
             }
             return render(request, self.template_name, context)
+
+
+
+
+
 
 
 
@@ -82,71 +85,56 @@ class SearchResults(TemplateView):
         """
         USE SERACH BOX
         """
+
         def post(self, request):
-            
+
             # CREATE A SESSION IF NOT EXISTING!!
             if not request.session.exists(request.session.session_key):
                 request.session.create()
             session_key = request.session.session_key
-        
-            current_user = "Anonymous"
+
+            current_user = request.user
             if not request.user.is_authenticated: 
-                current_user = "Anonymous"
+                current_user = request.user
             if request.user.is_authenticated:
-                current_user = request.user         
-            
-            
-            
-            
+                current_user = request.user
+
             results_collection = []
 
             all_films = film_models.Films.objects.all()
-        
-        
-        
-         
-        
-        
+
             s_string = request.POST['search_string']
-        
+
             for item in all_films:
-                if s_string.upper()  in (item.film_friendly_title).upper():
+                if s_string.upper() in (item.film_friendly_title).upper():
                     results_collection.append(item)
             total_items_found = len(results_collection)
-     
-     
- 
-     
+
             SCM = ShoppingCartModel.objects.filter(cart_owner=current_user)
             basket_item_count = SCM.count
+            
+ 
+ 
 
-     
-     
-     
-     
-     
-     
+
             context = {
-            
-            'results_collection': results_collection,
-            'total_items_found': total_items_found,
-            
-            'basket_item_count':basket_item_count,
-            
-            'current_user':current_user,
-            
-            
-            'SCM':SCM,
-               
+
+                'results_collection': results_collection,
+                'total_items_found': total_items_found,
+
+                'basket_item_count': basket_item_count,
+                'current_user': current_user,
+
+
+                'SCM': SCM,
+
             }
-            
-            
-            
-    
+
             return render(request, self.template_name, context)
 
-    except Exception as e :
-    
-        messages.info("An unexpected or illiegal error has occured - redirected to homepage ")
-        
-        redirect ( views.HomePageView.as_view)
+    except Exception as e:
+
+        messages.info(
+            "An unexpected or illiegal error has occured - redirected to homepage ")
+
+        redirect(views.HomePageView.as_view)
