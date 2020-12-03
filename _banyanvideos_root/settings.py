af728f5ad7bdb37dce500de45cc2dadf0751a60e
cofoeducistudent <cofoedu91@gmail.com>
2020-12-03 02:29:52 +0000

    code  refactor

--- _banyanvideos_root/settings.py
+++ _banyanvideos_root/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -317,6 +317,7 @@ EMAIL_HOST_PASSWORD='Alexkid1b'
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
