125a5c052d7ace1654bb632f1bdf40b8c7566359
cofoeducistudent <cofoedu91@gmail.com>
2020-12-16 00:42:06 +0000

    debug=false

--- _banyanvideos_root/settings.py
+++ _banyanvideos_root/settings.py
@@ -65,7 +65,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!@+@
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 
